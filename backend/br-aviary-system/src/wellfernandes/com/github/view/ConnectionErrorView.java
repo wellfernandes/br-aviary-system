09ee457a57ccc463fe83b2e1b79cf928907d3ce4
Welliton Fernandes Leal <wellfernandes@me.com>
2022-07-13 19:17:43 -0300

    code indentation only

--- backend/br-aviary-system/src/wellfernandes/com/github/view/ConnectionErrorView.java
+++ backend/br-aviary-system/src/wellfernandes/com/github/view/ConnectionErrorView.java
@@ -4,8 +4,8 @@ import javax.swing.JOptionPane;
 
 public class ConnectionErrorView {
 
-	public ConnectionErrorView(){
-		JOptionPane.showMessageDialog(null, "Serial communication not available.\n" 
-				+ "Please, select the correct port to connect.", "", 0);
+	public ConnectionErrorView() {
+		JOptionPane.showMessageDialog(null,
+				"Serial communication not available.\n" + "Please, select the correct port to connect.", "", 0);
 	}
 }
--- backend/br-aviary-system/src/wellfernandes/com/github/view/ConnectionOkView.java
+++ backend/br-aviary-system/src/wellfernandes/com/github/view/ConnectionOkView.java
@@ -1,10 +1,11 @@
 package wellfernandes.com.github.view;
+
 import javax.swing.ImageIcon;
 import javax.swing.JOptionPane;
 
 public class ConnectionOkView {
-	public ConnectionOkView(){
-		JOptionPane.showMessageDialog(null, "Successfully connected!","Connection Status",JOptionPane.OK_OPTION,
-		new ImageIcon(this.getClass().getResource("/wellfernandes/com/github/images/connectedSerial.png")));
+	public ConnectionOkView() {
+		JOptionPane.showMessageDialog(null, "Successfully connected!", "Connection Status", JOptionPane.OK_OPTION,
+				new ImageIcon(this.getClass().getResource("/wellfernandes/com/github/images/connectedSerial.png")));
 	}
 }
